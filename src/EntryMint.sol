b93e4c57437f31d9f9d850685d567118c81bee1c
Sam Oh <SamOh37@gmail.com>
2025-08-26 21:40:23 -0400

    initial contract

--- src/EntryMint.sol
+++ src/EntryMint.sol
@@ -2,3 +2,26 @@
 
 pragma solidity ^0.8.18;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// custom errros
+error OutOfSupply();
+error AlreadyMinted();
+
+contract EntryMint is ERC721, Ownable {
+    uint256 public totalSupply;
+    uint256 public constant MAX_SUPPLY = 1000;
+    mapping(address minter => uint256 mintedCount) public minted;
+
+    constructor() ERC721("EntryMint", "EMINT") Ownable(msg.sender) {}
+
+    function mint() external {
+        if (totalSupply >= MAX_SUPPLY) revert OutOfSupply();
+        if (minted[msg.sender] >= 1) revert AlreadyMinted();
+
+        totalSupply++;
+        minted[msg.sender]++;
+        _safeMint(msg.sender, totalSupply);        
+    }
+}
