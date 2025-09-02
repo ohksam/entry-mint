31d9730a6f199e79301b4d6994c0ff58c3daea3b
Sam Oh <SamOh37@gmail.com>
2025-09-01 21:49:14 -0400

    install router, create placeholder HomePage component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,49 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// function App() {
+//   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
 
-export default App
+// export default App
 
 import { WagmiProvider, createConfig } from 'wagmi'
 import { sepolia } from 'wagmi/chains';
-import { createPublicClient, http } from 'viem';
+import { createClient, http } from 'viem';
 import { BrowserRouter as Router, Routes, Route } from 'react-router';
- 
+
+import HomePage from './components/HomePage';
+
 const config = createConfig({
-  publicClient: createPublicClient({
-    chain: sepolia,
-    transport: http()
-  }),
+  chains: [sepolia],
+  client({ chain }) {
+    return createClient({ chain, transport: http()})
+  }
 })
  
 function App() {
@@ -58,3 +60,5 @@ function App() {
     </WagmiProvider>
   )
 }
+
+export default App;
---
+++ frontend/src/components/HomePage/index.tsx
@@ -0,0 +1,13 @@
+// imports
+
+// interfaces (probably not needed for home)
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to EntryMint!</h1>
+        </div>
+    )
+}
+
+export default HomePage;
